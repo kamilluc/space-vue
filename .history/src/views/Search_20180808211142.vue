0fe16db7eecd13ef327f59d38247a3be88962e46
kamilluc <kamil.luc@outlook.com>
2018-08-08 21:33:03 +0200

    api

---
+++ .history/src/views/Search_20180808211142.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <label for="search">Search</label>
+      <input type="text" id="search"
+      name="search"
+      v-model="searchValue"
+      @input="handleInput"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchValue: ""
+    };
+  },
+  methods: {
+    handleInput(e) {
+      console.log(this.searchValue);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+}
+
+.search {
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    font-family: Montserrat, sans-serif;
+  }
+
+  input {
+    height: 50px;
+    border: 0;
+    border-bottom: 1px solid black;
+  }
+}
+</style>
---
+++ .history/src/views/Search_20180808212555.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <label for="search">Search</label>
+      <input type="text" id="search"
+      name="search"
+      v-model="searchValue"
+      @input="handleInput"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import debounce from "lodash.debounce";
+
+const API = "https://images-api.nasa.gov/search?q=";
+
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchValue: ""
+    };
+  },
+  methods: {
+    // handleInput(e) {
+    //   axios
+    //     .get(`${API}${this.searchValue}&media_type=image`)
+    //     .then(res => {
+    //       console.log(res);
+    //     })
+    //     .catch(err => {
+    //       console.log(err);
+    //     });
+    // }
+
+    handleInput: debounce(e => {
+      axios
+        .get(`${API}${this.searchValue}&media_type=image`)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }, 800)
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+}
+
+.search {
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    font-family: Montserrat, sans-serif;
+  }
+
+  input {
+    height: 50px;
+    border: 0;
+    border-bottom: 1px solid black;
+  }
+}
+</style>
